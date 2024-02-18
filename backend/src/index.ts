66e046179a921917bc5e9db4a0b0041747c867b7
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-02-18 20:37:38 +0530

    feat: add express session

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 // Source the env file
 import 'dotenv/config'
 import express, { Express } from 'express'
-import { PORT } from './constants'
+import { PORT, SESSIONKEY } from './constants'
 // Middleware
 import { loggerMiddleware } from './middleware/logger.middleware'
 import { errorsMiddleware } from './middleware/error.middleware'
 // Routes
 import { helloRouter } from './routes/hello.route'
 import { postsRouter } from './routes/posts.route'
+import session from 'express-session'
 
 const app: Express = express()
 
@@ -16,6 +17,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(loggerMiddleware)
 app.use(errorsMiddleware)
+app.use(session({
+    secret: SESSIONKEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24 * 1 // 1 day
+    },
+}))
 
 // Add routes
 app.use('/hello', helloRouter)
