f9f996c9afbedf030977abfed3de9fcc5669d7a4
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-02-20 20:16:01 +0530

    WIP: add posts route

--- backend/src/routes/posts.route.ts
+++ backend/src/routes/posts.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { HttpCodes } from "../types/HttpCodes";
 import { CustomResponse } from "../types/CustomResponse";
-import { getAllPosts } from "../service/posts.service";
+import { createPost, getAllPosts, getMyPosts } from "../service/posts.service";
 
 export const postsRouter = Router();
 
@@ -24,4 +24,51 @@ postsRouter.get("/all", async (_, res) => {
         data: allPosts.data
     }
     return res.status(HttpCodes.OK).json(response);
+})
+
+postsRouter.get("/my", async (req, res) => {
+    const authorEmail = req.session.email as string;
+
+    const myPosts = await getMyPosts(authorEmail);
+
+    if (myPosts.error) {
+        const response: CustomResponse = {
+            error: true,
+            message: "Error retrieving posts",
+            data: null
+        }
+        return res.status(HttpCodes.INTERNAL_SERVER_ERROR).json(response);
+    }
+
+    const response: CustomResponse = {
+        error: false,
+        message: "My posts retrieved successfully",
+        data: myPosts.data
+    }
+    return res.status(HttpCodes.OK).json(response);
+})
+
+postsRouter.post("/create", async (req, res) => {
+    const data = req.body;
+    const authorEmail = req.session.email as string;
+
+    data["authorEmail"] = authorEmail;
+
+    const crePost = await createPost(data);
+
+    if (crePost.error) {
+        const response: CustomResponse = {
+            error: true,
+            message: "Error creating post",
+            data: null
+        }
+        return res.status(HttpCodes.INTERNAL_SERVER_ERROR).json(response);
+    }
+
+    const response: CustomResponse = {
+        error: false,
+        message: "Post created successfully",
+        data: crePost.data
+    }
+    return res.status(HttpCodes.CREATED).json(response);
 })
