8fa9a658973705f867f127c0ebfd4a67707bbf55
pks03 <115401101+pks03@users.noreply.github.com>
2024-02-26 00:23:38 +0530

    check

--- backend/src/routes/gauth.route.ts
+++ backend/src/routes/gauth.route.ts
@@ -5,11 +5,13 @@ export const gauthRouter = Router();
 
 gauthRouter.get("/", async (req, res) => {
     const appUser = await googleOAuthHandler(req);
-
+    console.log({appUser});
     if (appUser.error) {
+        console.log("new");
         res.redirect('http://localhost:3000/user/signup');
         return;
     } else {
+        console.log("old");
         res.redirect('http://localhost:3000');
         return;
     }
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -7,9 +7,11 @@ export const userRouter = Router();
 
 userRouter.post("/signup", async (req, res) => {
     const data = req.body;
-    const userEmail = req.session.email;
-
+    console.log(req);
+    const userEmail = req.session.email as string;
+    console.log(userEmail);
     data["email"] = userEmail;
+    console.log(data);
     const createUser = await newUser(data);
 
     if (createUser.error) {
