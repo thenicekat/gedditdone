67f82176ea1c285283773c243f3e494f25c88ac2
Manan <manangupta03@gmail.com>
2024-02-26 23:35:42 +0530

    backend Fix

--- backend/src/service/user.service.ts
+++ backend/src/service/user.service.ts
@@ -41,3 +41,43 @@ export const newUser = async (user: {
         }
     }
 }
+
+export const updatedUser = async (user: {
+    name: string,
+    email: string,
+    phoneNumber: string,
+}): Promise<CustomReturn<User>> => {
+    if (!user.name) return {
+        error: true,
+        data: "User name is required to update profile."
+    }
+
+    if (!user.phoneNumber) return {
+        error: true,
+        data: "Phone Number is required to update profile."
+    }
+
+    if (!user.email) return {
+        error: true,
+        data: "Email is required to update profile."
+    }
+
+    try {
+        let updatedUser = await prisma.user.update({
+            where: { email: user.email },
+            data: {
+                name: user.name,
+                phoneNumber: user.phoneNumber,
+            },
+        })
+        return {
+            error: false,
+            data: updatedUser
+        };
+    } catch (error) {
+        return {
+            error: true,
+            data: null
+        }
+    }
+}
--- backend/tests/user.test.ts
+++ backend/tests/user.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect } from "@jest/globals";
 import { prismaMock } from "./_mockdb";
 import { newUser } from "../src/service/user.service";
-
+import { updatedUser } from "../src/service/user.service";
 
 describe("Create a new user", () => {
     it("should create a new user", () => {
@@ -69,6 +69,76 @@ describe("Create a new user", () => {
         expect(newUser(user)).resolves.toEqual({
             error: true,
             data: "Phone Number is required to create profile."
+        });
+    })
+})
+
+describe ("Update user profile", () => {
+    it("update user profile", () => {
+        const user = {
+            id: "1",
+            name: "name",
+            email: "email",
+            phoneNumber: "phoneNumber",
+            karmaPoints: 0
+        }
+
+        prismaMock.user.update.mockResolvedValue(user);
+
+        expect(updatedUser(user)).resolves.toEqual({
+            error: false,
+            data: user
+        });
+    });
+
+    it("should return an error if email is not given", () => {
+        const user = {
+            id: "1",
+            name: "name",
+            email: "",
+            phoneNumber: "phoneNumber",
+            karmaPoints: 0
+        }
+
+        prismaMock.user.update.mockResolvedValue(user);
+
+        expect(updatedUser(user)).resolves.toEqual({
+            error: true,
+            data: "Email is required to update profile."
+        });
+    })
+
+    it("should return an error if name is not given", () => {
+        const user = {
+            id: "1",
+            name: "",
+            email: "email",
+            phoneNumber: "phoneNumber",
+            karmaPoints: 0
+        }
+
+        prismaMock.user.update.mockResolvedValue(user);
+
+        expect(updatedUser(user)).resolves.toEqual({
+            error: true,
+            data: "User name is required to update profile."
+        });
+    })
+
+    it("should return an error if phone number is not given", () => {
+        const user = {
+            id: "1",
+            name: "name",
+            email: "email",
+            phoneNumber: "",
+            karmaPoints: 0
+        }
+
+        prismaMock.user.update.mockResolvedValue(user);
+
+        expect(updatedUser(user)).resolves.toEqual({
+            error: true,
+            data: "Phone Number is required to update profile."
         });
     })
 })
