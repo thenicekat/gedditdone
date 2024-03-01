aa91e6ecc7a2f3acfaa145c1188cb8290a01cd5c
Saphereye <adarshdas950@gmail.com>
2024-03-01 23:33:47 +0530

    feat: Added feature to get all data from backend, now shows 'my' posts data

--- frontend/app/user/transactions/page.tsx
+++ frontend/app/user/transactions/page.tsx
@@ -7,84 +7,39 @@ import axios from "axios";
 import { siteConfig } from "@/config/site";
 import { set } from "react-hook-form";
 
-interface UserData {
-    id: string;
-    name: string;
-    email: string;
-    phoneNumber: string;
-    karmaPoints: number;
-    posts: Post[];
-    requests: Request[];
-}
-
-interface Post {
-    id: string;
-    authorId: string;
-    costInPoints: number;
-    source: string;
-    destination: string;
-    service: string;
-    requests: Request[];
-}
-
-interface Request {
-    id: string;
-    status: string;
-    senderId: string;
-    postId: string;
-}
+import { User, Post } from "@/types/index";
 
 export default function ShowTransactions() {
-    const transactions = [
-        {
-            authorName: "Divyateja",
-            source: "Mess 1",
-            destination: "V335",
-            service: "Pick up: Food",
-            costInPoints: 2
-        },
-    ];
-
     const [error, setError] = useState<string | null>(null);
-    const [userData, setUserData] = useState<UserData | null>(null);
-
-    const fetchUserData = async () => {
+    const [userData, setUserData] = useState<User | null>(null);
+    const [userPosts, setUserPosts] = useState<Post[]>([]);
+    
+    const fetchUserPosts = async () => {
         try {
             const response = await axios.get(
-                siteConfig.server_url + "/user/transactions",
+                siteConfig.server_url + "/post/my",
                 {
                     params: {},
                     withCredentials: true,
                 }
             );
 
-            setUserData(response.data.data);
+            setUserPosts(response.data.data);
             setError(null);
         } catch (err) {
-            console.error("Error fetching user data:", err);
-            setError("Error fetching user data.");
+            console.error("Error fetching user posts:", err);
+            setError("Error fetching user posts.");
         }
     };
 
     useEffect(() => {
-        fetchUserData();
+        fetchUserPosts();
     }, []);
 
     return (
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
             <div className="inline-block max-w-lg text-center justify-center">
                 <h1 className={title({ color: "green", size: "md" })}>Geddit.&nbsp;</h1>
-                {userData && (
-                    <>
-                        <p>ID: {userData.id}</p>
-                        <p>Name: {userData.name}</p>
-                        <p>Email: {userData.email}</p>
-                        <p>Phone Number: {userData.phoneNumber}</p>
-                        <p>Karma Points: {userData.karmaPoints}</p>
-                        <p>Posts: {userData.posts.length}</p>
-                        <p>Requests: {userData.requests.length}</p>
-                    </>
-                )}
                 <p>{error}</p>
                 <h1 className={title({
                     size: "md"
@@ -94,35 +49,23 @@ export default function ShowTransactions() {
             </div>
 
             <div>
-                <Table aria-label="Example static collection table">
-                    <TableHeader>
-                        <TableColumn>Post content</TableColumn>
-                        <TableColumn>Transaction Status</TableColumn>
-                        <TableColumn>The guy who accepted it</TableColumn>
-                        <TableColumn>Points</TableColumn>
-                    </TableHeader>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell>Sus stuff</TableCell>
-                            <TableCell><Chip color="success">Completed</Chip></TableCell>
-                            <TableCell>Divyateja</TableCell>
-                            <TableCell>2</TableCell>
+            <Table aria-label="Example static collection table">
+                <TableHeader>
+                    <TableColumn>Post content</TableColumn>
+                    <TableColumn>The guy who accepted it</TableColumn>
+                    <TableColumn>Points</TableColumn>
+                </TableHeader>
+                <TableBody>
+                    {userPosts.map((post, index) => (
+                        <TableRow key={index}>
+                            <TableCell>{post.source} to {post.destination}</TableCell>
+                            <TableCell>{post.author ? <Chip color="success">post.author.name</Chip> : <Chip color="default">Not accepted</Chip>}</TableCell>
+                            <TableCell>{post.costInPoints}</TableCell>
                         </TableRow>
-                        <TableRow>
-                            <TableCell>Useful stuff</TableCell>
-                            <TableCell><Chip color="warning">In Progess</Chip></TableCell>
-                            <TableCell>Divyateja</TableCell>
-                            <TableCell>2</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Life stuff</TableCell>
-                            <TableCell><Chip color="danger">Incomplete</Chip></TableCell>
-                            <TableCell><Chip color="default">No Takers</Chip></TableCell>
-                            <TableCell>2</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </div>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
         </section>
     );
 }
