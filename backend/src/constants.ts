c6451a3ebb8291aff1b4fe2f843a5c538f198398
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-02 23:00:18 +0530

    feat: change constant names

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -2,5 +2,5 @@ export const PORT: number = 5000;
 export const __prod__ = process.env.NODE_ENV as string === "production";
 export const __test__ = process.env.NODE_ENV as string === "test";
 export const SESSIONKEY: string = process.env.SESSIONKEY as string;
-export const google_client_id = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-export const google_redirect_uri = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URI;
+export const google_client_id = process.env.GOOGLE_CLIENT_ID;
+export const google_redirect_uri = process.env.GOOGLE_OAUTH_REDIRECT_URI;
