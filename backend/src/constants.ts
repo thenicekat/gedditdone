ab7a98e6a38197f0eda82a0f49f06663a1bf7793
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-02 22:11:09 +0530

    feat: change constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -4,4 +4,3 @@ export const __test__ = process.env.NODE_ENV as string === "test";
 export const SESSIONKEY: string = process.env.SESSIONKEY as string;
 export const google_client_id = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 export const google_redirect_uri = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URI;
-export const google_client_secret = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_SECRET;
