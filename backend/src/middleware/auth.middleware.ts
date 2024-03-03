a611f3c56cb28b89ce70dbfe04e506b8c19177ae
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-03 12:20:51 +0530

    feat: add logger to auth middleware

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,8 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { HttpCodes } from "../types/HttpCodes";
+import { logger } from "../utils/logger";
 
 export const validateMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.session.email)
+    if (!req.session.email) {
+        logger.error("Unauthorized, Session has: ", req.session.email)
         res.status(HttpCodes.UNAUTHORIZED).json({ error: true, message: "Unauthorized" });
+    }
     else next();
 }
