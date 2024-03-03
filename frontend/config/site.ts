ecf3426e09ad9a5925b1022f0b0540d6cb92822a
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-03 10:26:18 +0530

    fix: vercel env mismatch

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -2,7 +2,7 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
 	name: "Geddit",
-	server_url: process.env.NEXT_PUBLIC_SERVER_ENDPOINT,
+	server_url: process.env.NEXT_PUBLIC_SERVER_URL,
 	description: "Get things done.",
 	navItems: [
 		{ label: "Dashboard", href: "/user/dashboard" },
