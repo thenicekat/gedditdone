87d559d802078d9cdf4a00dc3d00eda97dee521e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-04 19:50:12 +0530

    feat: add requests route

---
+++ backend/src/routes/requests.route.ts
@@ -0,0 +1,74 @@
+import { Router } from "express";
+import { CustomResponse } from "../types/CustomResponse";
+import { createRequest, getMyRequests, getRequestsForPost } from "../service/requests.service";
+import { HttpCodes } from "../types/HttpCodes";
+
+export const requestsRouter = Router()
+
+requestsRouter.get("/post", async (req, res) => {
+    const postId = req.query.postId as string;
+
+    const requestsPerPost = await getRequestsForPost(postId);
+
+    if (requestsPerPost.error) {
+        const response: CustomResponse = {
+            error: true,
+            message: "Error retrieving requests",
+            data: null
+        }
+        return res.status(HttpCodes.INTERNAL_SERVER_ERROR).json(response);
+
+    }
+
+    const response: CustomResponse = {
+        error: false,
+        message: "All posts retrieved successfully",
+        data: requestsPerPost.data
+    }
+    return res.status(HttpCodes.OK).json(response);
+})
+
+requestsRouter.get("/my", async (req, res) => {
+    const authorEmail = req.session.email as string;
+
+    const myRequests = await getMyRequests(authorEmail);
+
+    if (myRequests.error) {
+        const response: CustomResponse = {
+            error: true,
+            message: "Error retrieving your requests",
+            data: null
+        }
+        return res.status(HttpCodes.INTERNAL_SERVER_ERROR).json(response);
+    }
+
+    const response: CustomResponse = {
+        error: false,
+        message: "Your requests retrieved successfully",
+        data: myRequests.data
+    }
+    return res.status(HttpCodes.OK).json(response);
+})
+
+requestsRouter.post("/create", async (req, res) => {
+    const postId = req.body.postId as string;
+    const authorEmail = req.session.email as string;
+
+    const creRequest = await createRequest(postId, authorEmail);
+
+    if (creRequest.error) {
+        const response: CustomResponse = {
+            error: true,
+            message: creRequest.data as string,
+            data: null
+        }
+        return res.status(HttpCodes.INTERNAL_SERVER_ERROR).json(response);
+    }
+
+    const response: CustomResponse = {
+        error: false,
+        message: "Request created successfully",
+        data: creRequest.data
+    }
+    return res.status(HttpCodes.CREATED).json(response);
+})
