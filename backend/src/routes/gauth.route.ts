a3e1654e007bf4c798e78fbb63f661325f7c61db
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-31 17:33:28 +0530

    fix: minor bug fixes

--- backend/src/routes/gauth.route.ts
+++ backend/src/routes/gauth.route.ts
@@ -4,9 +4,6 @@ import { logger } from "../utils/logger";
 import { FRONTEND_URL } from "../constants";
 
 export const gauthRouter = Router();
-// function isuser(obj: any): obj is  {
-//     return obj && typeof obj === 'object' && 'role' in obj;
-// }
 
 gauthRouter.get("/", async (req, res) => {
     const appUser = await googleOAuthHandler(req);
@@ -22,16 +19,16 @@ gauthRouter.get("/", async (req, res) => {
         res.redirect(FRONTEND_URL + '/user/create');
         return;
     } else {
-        if(appUser.data){
-            if(typeof appUser.data !=='string' && appUser.data.role ==="admin"){
-            logger.info("Admin login");
-            res.redirect(FRONTEND_URL+'/admin/home');
-            return;
+        if (appUser.data) {
+            if (typeof appUser.data !== 'string' && appUser.data.role === "admin") {
+                logger.info("Admin login");
+                res.redirect(FRONTEND_URL + '/admin/home');
+                return;
             }
-        }else{
-        logger.info("User already exists");
-        res.redirect(FRONTEND_URL);
-        return;
+        } else {
+            logger.info("User already exists");
+            res.redirect(FRONTEND_URL);
+            return;
         }
     }
 });
